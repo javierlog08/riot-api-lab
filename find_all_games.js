6592ef23a15f524d894f15ee207b839339fccfb6
unknown <javierlog08@gmail.com>
2016-07-26 18:39:17 -0500

    Changes about project, get all player matchs

--- find_all_games.js
+++ find_all_games.js
@@ -4,20 +4,8 @@ var config = {
     requestLimit: '10 requests every 10 seconds'
 }
 
-//var chid = 421; // resaki
-//var chid  = 76; //udyr
-//var chid = 62;//wukong
-//var summoner = 321694; // happy dead
-//var chid = 84; // akali
-//var chid = 107; // rengar
-var chid = 121; //kha six
-//var chid = 64; // Lee sin
-//var chid = 79; //gragas
-//var chid = 67; //vayne
-//var chid = 103; // ahri
-
 //var summoner = 21944884; //OmarGod
-//var summoner = 331637; // issho
+var summoner = 331637; // issho
 //var summoner = 20947579; //lyon oddie
 //var summoner = 32932398; // contractz
 //var summoner = 612384; //Litopao
@@ -25,13 +13,14 @@ var chid = 121; //kha six
 //var summoner = 321701; //RobSee
 //var summoner = 29420083; //radara na
 //var summoner = 9652867; //cuss kr
-var summoner =76993173; //Nightweebxd
+//var summoner =76993173; //Nightweebxd
 
-var region = 'na';
+var region = 'lan';
 
 // Finding Reksai match
-var matchListUrl = 'https://'+region+'.api.pvp.net/api/lol/'+region+'/v2.2/matchlist/by-summoner/'+summoner+'?championIds='+chid+'&rankedQueues=TEAM_BUILDER_DRAFT_RANKED_5x5,RANKED_SOLO_5x5&seasons=PRESEASON3,SEASON3,PRESEASON2014,SEASON2014,PRESEASON2015,SEASON2015,PRESEASON2016,SEASON2016';
+var matchListUrl = 'https://'+region+'.api.pvp.net/api/lol/'+region+'/v2.2/matchlist/by-summoner/'+summoner+'?rankedQueues=TEAM_BUILDER_DRAFT_RANKED_5x5,RANKED_SOLO_5x5&seasons=PRESEASON3,SEASON3,PRESEASON2014,SEASON2014,PRESEASON2015,SEASON2015,PRESEASON2016,SEASON2016';
 var matchUrl = 'https://'+region+'.api.pvp.net/api/lol/'+region+'/v2.2/match/';
+var champUrl = 'https://global.api.pvp.net/api/lol/static-data/lan/v1.2/champion/';
 var riotApi = require('riot-api-client')(config);
 var _ = require('underscore');
 
@@ -52,14 +41,19 @@ riotApi.get(matchListUrl, function callback(err, matchList){
                     var indx  = (len > 0)? len-1 :0 ;
                     riotApi.get(matchUrl+matchList.matches[indx].matchId, function callback(err,matchInfo){
                         if(matchInfo != null) {
-                            var tmp_match = _.findWhere(matchInfo.participants,{championId:chid});
-                            data.push(tmp_match.stats);
+                            var tmp_match = _.findWhere(matchInfo.participants,{championId:matchList.matches[indx].champion});
+                            var stats = {};
+                            stats = tmp_match.stats;
+                            riotApi.get(champUrl+matchList.matches[indx].champion,function(err,champInfo) {
+                                stats.champion = champInfo.name;
+                                data.push(stats);
+                            });
                         }
                         len--;
                         console.log("Progress -> %"+len);
                         if(len <= 0){
                             jsonfile.writeFile(file, data, function (err) {
-                                exec("convert2csv -i data.json -o "+chid+".csv", function (error, stdout, stderr) {
+                                exec("convert2csv -i data.json -o "+summoner+".csv", function (error, stdout, stderr) {
                                     if (error !== null) {
                                         console.log('exec error: ' + error);
                                     }
